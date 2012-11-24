46984e27d440b7f7632b402c6174ed934e4cc2a1
Brian Ploeckelman <ploeckelman@wisc.edu>
2012-11-24 14:26:06 -0600

    Exposed level size properties in Level object

--- scripts/level.js
+++ scripts/level.js
@@ -7,11 +7,26 @@ function Level (game) {
     this.grid1 = null;
     this.grid2 = null;
     this.cells = null;
+    this.size  = null;
+    this.structures = null;
 
 
     // Private variables ------------------------------------------------------
-    var self = this,
-        LEVEL_SIZE = {
+    var self = this;
+
+
+    // Level methods ----------------------------------------------------------
+    this.update = function () {
+        for (var i = 0; i < self.structures.length; ++i) {
+            self.structures[i].update();
+        }
+    };
+
+
+    // Constructor ------------------------------------------------------------
+    (this.init = function (level) {
+        // Specify level sizes
+        level.size = {
             width:  1000,
             height: 1000,
             cellw:  10,
@@ -20,30 +35,22 @@ function Level (game) {
             ycells: 100
         };
 
-    // Level methods ----------------------------------------------------------
-    this.update = function () {
-        // ...
-    };
-
-
-    // Constructor ------------------------------------------------------------
-    (this.init = function (level) {
         // Create level meshes
         level.grid1 = new THREE.Mesh(
-            new THREE.PlaneGeometry(LEVEL_SIZE.width, LEVEL_SIZE.height,
-                                    LEVEL_SIZE.xcells, LEVEL_SIZE.ycells),
+            new THREE.PlaneGeometry(level.size.width, level.size.height,
+                                    level.size.xcells, level.size.ycells),
             new THREE.MeshBasicMaterial({ color: 0x112211, wireframe: true })
         );
 
         level.grid2 = new THREE.Mesh(
-            new THREE.PlaneGeometry(LEVEL_SIZE.width, LEVEL_SIZE.height,
-                                    LEVEL_SIZE.xcells / 4, LEVEL_SIZE.ycells / 4),
+            new THREE.PlaneGeometry(level.size.width, level.size.height,
+                                    level.size.xcells / 4, level.size.ycells / 4),
             new THREE.MeshBasicMaterial({ color: 0x115511, wireframe: true })
         );
 
         // Reposition the grid so its bottom left corner at (0,0,0)
-        level.grid1.position.set(LEVEL_SIZE.width / 2, LEVEL_SIZE.height / 2, 0);
-        level.grid2.position.set(LEVEL_SIZE.width / 2, LEVEL_SIZE.height / 2, 0.1);
+        level.grid1.position.set(level.size.width / 2, level.size.height / 2, 0);
+        level.grid2.position.set(level.size.width / 2, level.size.height / 2, 0.1);
 
         // Add the meshes to the scene
         game.scene.add(level.grid1);
@@ -52,17 +59,21 @@ function Level (game) {
 
         // Create level cells
         level.cells = [];
-        for(var y = 0; y < LEVEL_SIZE.ycells; ++y) {
+        for(var y = 0; y < level.size.ycells; ++y) {
             level.cells.push([]);
-            for(var x = 0; x < LEVEL_SIZE.xcells; ++x) {
+            for(var x = 0; x < level.size.xcells; ++x) {
                 level.cells[y].push({ // cell[y][x]
                     indices: new THREE.Vector2(x,y),
                     pos: new THREE.Vector3(
-                            x * LEVEL_SIZE.cellw,
-                            y * LEVEL_SIZE.celly, 0)
+                            x * level.size.cellw,
+                            y * level.size.celly, 0)
                 });
             }
         }
+
+        // Initialize the structures container
+        // TODO: add initial structures
+        level.structures = [];
 
         console.log("Level initialized.");
     })(self);
