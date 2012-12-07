9ff3f223924bb664c3108d60f75c136d7e4bc8a1
Brian Ploeckelman <ploeckelman@wisc.edu>
2012-12-06 23:03:24 -0600

    Added pulse for artifact, increases frequency as it approaches death

--- scripts/artifact.js
+++ scripts/artifact.js
@@ -7,6 +7,7 @@ function Artifact (level, game) {
     this.mesh   = null;
     this.clock  = null;
     this.health = null;
+    this.pulse  = null;
 
     // Private variables ------------------------------------------------------
     var self = this,
@@ -25,9 +26,15 @@ function Artifact (level, game) {
         self.mesh.rotation.y += 0;
         self.mesh.rotation.z += Math.cos(t / 1000) * 300;
 
+        // TODO: these updates should be done in a takeDamage() function
         // Update color based on health (goes more red as damaged)
         var green = self.health / 100;
         self.mesh.material.color.setRGB(1, green, 0);
+        // Increase pulse rate as health drops
+        // TODO: switch to an easing function for calculating pulse time rate 
+        self.pulse.time = 2000 * self.health / 100 + 10;
+        self.pulse.tweenIn.to({ scale: self.pulse.maxScale }, self.pulse.time);
+        self.pulse.tweenOut.to({ scale: self.pulse.minScale }, self.pulse.time);
     };
 
 
@@ -56,8 +63,8 @@ function Artifact (level, game) {
                 2 * level.size.cellh),
             new THREE.MeshLambertMaterial({
                 color: 0xffff00,
-                //transparent: true,
-                //blending: THREE.AdditiveBlending
+                transparent: true,
+                blending: THREE.AdditiveBlending
             })
         );
 
@@ -74,6 +81,33 @@ function Artifact (level, game) {
 
         // Set initial health
         artifact.health = 100;
+
+        // Setup pulse tweens
+        artifact.pulse = { time: 500, minScale: 0.75, maxScale: 1.0, tweenIn: null, tweenOut: null };
+
+        artifact.pulse.tweenIn = new TWEEN.Tween({ scale: artifact.pulse.minScale })
+            .to({ scale: artifact.pulse.maxScale }, artifact.pulse.time)
+            //.easing(TWEEN.Easing.Quadratic.InOut)
+            .onUpdate(function () {
+                artifact.mesh.scale.x = this.scale;
+                artifact.mesh.scale.y = this.scale;
+                artifact.mesh.scale.z = this.scale;
+            })
+            .onComplete(function () { this.scale = artifact.pulse.minScale; });
+
+        artifact.pulse.tweenOut = new TWEEN.Tween({ scale: artifact.pulse.maxScale })
+            .to({ scale: artifact.pulse.minScale }, artifact.pulse.time)
+            //.easing(TWEEN.Easing.Quadratic.InOut)
+            .onUpdate(function () {
+                artifact.mesh.scale.x = this.scale;
+                artifact.mesh.scale.y = this.scale;
+                artifact.mesh.scale.z = this.scale;
+            })
+            .onComplete(function () { this.scale = artifact.pulse.maxScale; });
+
+        artifact.pulse.tweenIn.chain(artifact.pulse.tweenOut);
+        artifact.pulse.tweenOut.chain(artifact.pulse.tweenIn);
+        artifact.pulse.tweenIn.start();
 
         console.log("Artifact initialized.");
     })(self);
